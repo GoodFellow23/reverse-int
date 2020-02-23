6a84b6770b4bb3487e2e66dcc608bcfce525d4ac
DZMITRY YATSKEVICH <53887281+GoodFellow23@users.noreply.github.com>
2020-02-23 15:00:41 +0300

    reverse-int: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let temp=0;
+    n= Math.abs(n);
+
+    while (n >= 10) 
+    {
+        temp=temp*10+n%10;
+        n=Math.trunc(n/10);
+    }
+        temp=temp*10+n;
+    return temp;
 }
